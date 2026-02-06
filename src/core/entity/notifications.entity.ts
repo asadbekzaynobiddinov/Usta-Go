758afd9d3c1823fe2d94ef5c9da3b6c25dadc3e5
asadbekzaynoiddinov <asadbekzaynobiddinov51@gmail.com>
2026-02-06 10:19:48 +0500

    Notifications: target_role

--- src/core/entity/notifications.entity.ts
+++ src/core/entity/notifications.entity.ts
@@ -1,10 +1,14 @@
 import { Entity, Column } from 'typeorm';
 import { BaseEntity } from 'src/common/database/BaseEntity';
+import { TargetRole } from 'src/common/enum';
 
 @Entity()
 export class Notifications extends BaseEntity {
   @Column({ nullable: false, type: 'uuid' })
   to: string;
+
+  @Column({ nullable: false, type: 'enum', enum: TargetRole })
+  target_role: TargetRole;
 
   @Column({ nullable: false })
   title: string;
